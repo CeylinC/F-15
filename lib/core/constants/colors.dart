a8a90b8b2cd50be38938f9f28d9210d70ffda2cf
Ceylin <caltepeceylin@gmail.com>
2023-06-17 23:39:35 +0300

    Update colors.dart

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -1,22 +1,19 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
-class AppColors{
-  static Color appThemeClr = Color(0xFF253F64);
-  static Color lightThemeClr = Color(0xFF4C8EA9);
-  static Color backgroundGreyThemeClr = Color(0xFFf6f6f6);
-  static Color shadowThemeClr = Color(0x00000029);
-  static Color redDarkThemeClr = Color(0xFF42130F);
-  static Color greenThemeClr = Color(0xFF008000);
-  static Color redThemeClr = Color(0xFFff0000);
-  static Color blueThemeClr = Color(0xFF0000ff);
-  static Color yellowThemeClr = Color(0xFFffa500);
-  static Color blackThemeClr = Colors.black;
-  static Color whiteThemeClr = Colors.white;
-  static Color greyThemeClr = Colors.grey;
-}
-
+// Renk Kodlarının başına k yazılsın
+const Color kAppThemeClr = Color(0xFF253F64);
+const Color kLightThemeClr = Color(0xFF4C8EA9);
+const Color kBackgroundGreyThemeClr = Color(0xFFf6f6f6);
+const Color kShadowThemeClr = Color(0x00000029);
+const Color kRedDarkThemeClr = Color(0xFF42130F);
+const Color kGreenThemeClr = Color(0xFF008000);
+const Color kRedThemeClr = Color(0xFFff0000);
+const Color kBlueThemeClr = Color(0xFF0000ff);
+const Color kYellowThemeClr = Color(0xFFffa500);
+const Color kBlackThemeClr = Colors.black;
+const Color kWhiteThemeClr = Colors.white;
+const Color kGreyThemeClr = Colors.grey;
 
 //GÜNCEL RENK KODLARI EKLENECEK !!!!
 
