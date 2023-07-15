2c66d50ad1939c384d0abf6c40f892127e73d3a0
Ceylin <caltepeceylin@gmail.com>
2023-07-15 16:07:14 +0300

    Update ad.dart

--- lib/core/components/ad.dart
+++ lib/core/components/ad.dart
@@ -8,65 +8,61 @@ class Ad extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: SizedBox(
-            width: cardWidth,
-            height: cardHeight,
-            child: Card(
-              elevation: 0,
-              shape: const BeveledRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10))),
-              child: Column(
+    return SizedBox(
+        width: cardWidth,
+        height: cardHeight,
+        child: Card(
+          elevation: 0,
+          shape: const BeveledRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(10))),
+          child: Column(
+            children: [
+              ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  child: SizedBox(
+                    width: cardWidth,
+                    height: 140,
+                    child: Image.network(
+                        "https://i.pinimg.com/236x/76/cd/fd/76cdfd5c95f05195d323c770af75324d.jpg",
+                        fit: BoxFit.cover),
+                  )),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: SizedBox(
+                  width: cardWidth - 20,
+                  child: const Text(
+                    "İlan Deneme",
+                    textAlign: TextAlign.start,
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ),
+              const Row(
                 children: [
-                  ClipRRect(
-                      borderRadius: const BorderRadius.all(Radius.circular(10)),
-                      child: SizedBox(
-                        width: cardWidth,
-                        height: 140,
-                        child: Image.network(
-                            "https://i.pinimg.com/236x/76/cd/fd/76cdfd5c95f05195d323c770af75324d.jpg",
-                            fit: BoxFit.cover),
-                      )),
+                  Icon(
+                    Icons.location_on,
+                    color: kPurpleColor,
+                  ),
                   Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    child: SizedBox(
-                      width: cardWidth - 20,
-                      child: const Text(
-                        "İlan Deneme",
-                        textAlign: TextAlign.start,
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ),
-                  ),
-                  const Row(
-                    children: [
-                      Icon(
-                        Icons.location_on,
-                        color: kPurpleColor,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 5),
-                        child: Text("İstanbul"),
-                      )
-                    ],
-                  ),
-                  const Row(
-                    children: [
-                      Icon(
-                        Icons.star,
-                        color: Colors.amber,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 5),
-                        child: Text("5.0"),
-                      )
-                    ],
+                    padding: EdgeInsets.only(left: 5),
+                    child: Text("İstanbul"),
                   )
                 ],
               ),
-            )),
-      ),
-    );
+              const Row(
+                children: [
+                  Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 5),
+                    child: Text("5.0"),
+                  )
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
